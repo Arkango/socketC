9d1958904099abc706ea9f4b41b26413575e6050
arkango <saracinoarcangelo@gmail.com>
2019-10-08 17:23:48 +0200

    copia di geek

--- client/main.c
+++ client/main.c
@@ -1,94 +1,41 @@
-//
-// Created by arcangelo on 10/7/19.
-//
-
+// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#define PORT 8080
 
-#define PORT 5000
-#define BUFFERSIZE 1024
-
-char* insertString(int i);
-
-int main(int argc,char const *argv[]){
-    int sock;
-    char buff[BUFFERSIZE];
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
     struct sockaddr_in serv_addr;
-
-    //creazione del socket
-    if((sock = socket(AF_INET, SOCK_STREAM,0) < 0)){
-        printf("\n Socket creation error\n");
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
         return -1;
     }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(PORT);
-    /*
+
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
-    }*/
+    }
 
-    connect(sock,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    /*if(connect(sock,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         printf("\nConnection Failed \n");
         return -1;
-    }*/
-
-    /*Ricevo dati dal server*/
-    int flag =0;
-    int times = 0;
-    ssize_t numBytesRcvd;
-
-    while(1){
-        numBytesRcvd = recv(sock,buff,BUFFERSIZE-1,0);
-        buff[numBytesRcvd] = '\0';
-        if(numBytesRcvd <= 0){
-            printf("Connection failed or closed prematurely,Exit...\n");
-            flag = 1;
-            break;
-        }
-        printf("From Server : %s", buff);
-        times += 1;
-        if (times == 3) {
-            printf("Client Exit...\n");
-            break;
-        }else if((strncmp(buff, "connessione avvenuta", 20)) == 0){
-
-            char *stringa_1 = insertString(times);
-
-            if (send(sock, stringa_1, strlen(stringa_1), 0) !=  strlen(stringa_1)) {
-                printf("send() sent a different number of bytes than expected\n");
-                flag = 1;
-                break;
-            }
-        }
-
     }
-
-
-    close(sock);
-    if(flag == 1){
-        return -1;
-    }
-
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
     return 0;
-}
-
-char* insertString(int i){
-    char *stringa_1;
-    char *name[2];
-    name[1] = "prima";
-    name[2] ="seconda";
-    printf("Inserire la %s stringa in input\n",name[i]);
-    scanf("%s",stringa_1);
-
-    return stringa_1;
-
 }
--- server/main.c
+++ server/main.c
@@ -1,173 +1,59 @@
-//
-// Created by arcangelo on 10/7/19.
-//
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <arpa/inet.h>
+// Server side C/C++ program to demonstrate Socket programming 
 #include <unistd.h>
-#include <ctype.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <sys/socket.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <sys/types.h>
-#include <time.h>
-
-#define PORT 5000
-#define BUFFERSIZE 1024
-#define MAXCONN 6
-
-void func(int sockfd);
-
-int main(int argc,char const *argv[]) {
-    int sock,connfd;
-    char buff[BUFFERSIZE];
-    struct sockaddr_in serv_addr,client;
-
-    //creazione del socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0) < 0)) {
-        printf("\n Socket creation error\n");
-        return -1;
-    }
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(buff, '0', sizeof(buff));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(PORT);
-
-    bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
-    // Binding newly created socket to given IP and verification
-    /*if ((bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("socket bind failed...\n");
-        close(sock);
-        exit(-1);
-    }*/
-
-    listen(sock, MAXCONN);
-
-    /*if ((listen(sock, MAXCONN)) != 0) {
-        printf("Listen failed...\n");
-        close(sock);
-        exit(-1);
-    }*/
-
-    int flag =0;
-    int len = sizeof(client);
-
-
-
-
-        int times = 1;
-        ssize_t numBytesRcvd;
-
-       /* while(1){
-
-          if(times == 0) {
-              char *ok_connect = "connessione avvenuta";
-              int connectLenght = sizeof(ok_connect);
-              if (send(connfd, ok_connect, connectLenght, 0) != connectLenght) {
-                  printf("send() sent a different number of bytes than expected\n");
-                  close(sock);
-                  close(connfd);
-                  exit(-1);
-              }
-          }
-
-
-
-            numBytesRcvd = recv(connfd,buff,BUFFERSIZE-1,0);
-            buff[numBytesRcvd] = '\0';
-            if(numBytesRcvd <= 0){
-                printf("Connection failed or closed prematurely,Exit...\n");
-                flag = 1;
-                break;
-            }
-
-            times += 1;
-            if (times == 1) {
-
-                char *newString;
-                int i=0;
-                for(i=0; i < numBytesRcvd; i++){
-                    newString += toupper(buff[i]);
-                }
-
-                if (send(sock, newString, strlen(buff), 0) !=  strlen(buff)) {
-                    printf("send() sent a different number of bytes than expected\n");
-                    flag = 1;
-                    break;
-                }
-
-
-
-            }else if(times == 2){
-
-                char *newString;
-                int i=0;
-                for(i=0; i < numBytesRcvd; i++){
-                    newString += tolower(buff[i]);
-                }
-
-                if (send(sock, newString, strlen(buff), 0) !=  strlen(buff)) {
-                    printf("send() sent a different number of bytes than expected\n");
-                    flag = 1;
-                    break;
-                }
-
-            }
-
-        }*/
-
-    connfd = accept(sock, (struct sockaddr*)&client, &len);
-    func(connfd);
-
-
-
-    close(sock);
-    if(flag == 1){
-        return -1;
-    }
-
-
-    return 0;
-}
-
-void func(int sockfd)
+#define PORT 8080
+int main(int argc, char const *argv[])
 {
-    char buff[BUFFERSIZE];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, BUFFERSIZE);
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
 
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, BUFFERSIZE);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
-
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
-}
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
+} 
